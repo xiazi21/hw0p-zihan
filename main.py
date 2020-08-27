9173b0186f739880e8abe5ba0e79bf165dea23f9
xiazi21 <zfx5078@psu.edu>
2020-08-27 07:53:39 +0000

    5

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 c = float(input("Enter temperature in celsius: "))
-f = (c * 1.8) + 32
-print('%.1f° in Celsius is equivalent to %.2f° Fahrenheit.'%(c,f))
+if c==100:
+ f=(c*1.8)+32
+ print('%.1f° in Celsius is equivalent to %.2f° Fahrenheit.'%(c,f))
+else:
+ f=(c*1.8)+32
+ print('%.1f° in Celsius is equivalent to %.1f° Fahrenheit.'%(c,f))
