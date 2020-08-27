4d7572538996cfd4e1d03c42a74939979c02025b
xiazi21 <zfx5078@psu.edu>
2020-08-27 06:54:19 +0000

    1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 c = float(input("Enter temperature in celsius:"))
 f = (c * 1.8) + 32
 a ='°'
-print(c,a,'in Celsius is equivalent to',f,a,'Fahrenheit.')
+print(c,'°',' in Celsius is equivalent to',f,a,'Fahrenheit.')
 
 
