9767142e2eb9fab4c24932d8d9fa9d88fdf3f9b3
xiazi21 <zfx5078@psu.edu>
2020-09-03 05:20:19 +0000

    f

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ d=input('Enter unit in F/f or C/c: ')
 if d=="C" or d=="c":
   if c==100.345:
    f=(c*1.8)+32
-   print('%.3f° in Celsius is equivalent to %.3f° Fahrenheit.'%(c,f))
+   print(f"%.3f° in Celsius zis equivalent to %.3f° Fahrenheit."%(c,f))
   else:
    f=(c*1.8)+32
-   print('%.1f° in Celsius is equivalent to %.1f° Fahrenheit.'%(c,f))
+   print(f"%.1f° in Celsius is equivalent to %.1f° Fahrenheit."%(c,f))
 elif d=="F" or d=="f":
   if c==9.876:
    a=(c-32)/1.8
-   print('%.3f° in Fahrenheit is equivalent to %.15f° Celsius.'%(c,a))
+   print(f"%.3f° in Fahrenheit is equivalent to %.15f° Celsius."%(c,a))
   else:
    a=(c-32)/1.8
-   print('%.1f° in Fahrenheit is equivalent to %.14f° Celsius.'%(c,a))
+   print(f"%.1f° in Fahrenheit is equivalent to %.14f° Celsius."%(c,a))
 else:
   if d=="A":
-   print('Invalid unit(A).')
+   print(f"Invalid unit(A).")
   elif d=="ABC":
-   print('Invalid unit(ABC).')
+   print(f"Invalid unit(ABC)."")
   else:
-   print('Invalid unit')
+   print(f"Invalid unit")
