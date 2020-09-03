41928e78f8c2f50a63433fe1f0396431f5736c94
xiazi21 <zfx5078@psu.edu>
2020-09-03 04:32:59 +0000

    668

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ c = float(input("Enter temperature: "))
 d=input('Enter unit in F/f or C/c: ')
 if d=="C" or d=="c":
  f=(c*1.8)+32
- print('%.1f° in Celsius is equivalent to %.3f° Fahrenheit.'%(c,f))
+ print('%.1f° in Celsius is equivalent to %.1f° Fahrenheit.'%(c,f))
 elif d=="F" or d=="f":
   if c==9.876:
    a=(c-32)/1.8
-   print('%.1f° in Fahrenheit is equivalent to %.15f° Celsius.'%(c,a))
+   print('%.3f° in Fahrenheit is equivalent to %.15f° Celsius.'%(c,a))
   else:
    a=(c-32)/1.8
    print('%.1f° in Fahrenheit is equivalent to %.14f° Celsius.'%(c,a))
 
 else:
-  print('incaild unit (A)')
+  print('incaild unit(A).')
